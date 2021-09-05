fcb627d2d0154f0d06ad37b1d911b07f9f9c7917
Marek Dmowski <88617623+Mardmoo@users.noreply.github.com>
2021-09-05 14:02:31 +0200

    Update rzut_moneta.py

--- Zjazd_2/rzut_moneta.py
+++ Zjazd_2/rzut_moneta.py
@@ -1,3 +1,4 @@
+#sposob z zajec
 import random
 
 def rzut_moneta():
@@ -26,3 +27,23 @@ def rzut_moneta_n_razy(n):
 
 rzut_moneta_n_razy(10)
         
+#sposob po optymalizacji
+import random
+def rzuc_moneta():
+    return random.choice([0,1])
+def policz_orly(n):
+    current_run, longest_run = 0
+    orzel_w_poprzednim_rzucie = None
+    for i in range(n):
+        #Python traktuje liczbe 1 jako True, liczbe 0 jako False
+        wypadl_orzel = rzuc_moneta()
+        if wypadl_orzel and (orzel_w_poprzednim_rzucie or orzel_w_poprzednim_rzucie is None):
+            # Pytanie: dlaczego jest orzel_w_poprzednim_rzucie is None?
+            # Odpowiedz: Bo przed rozpoczeciem rzucania moneta, nie mozemy zdefiniowac czym byl poprzedni rzut.
+            # Jest to wspomniany na zajeciach 'warunek startowy'
+            current_run += 1
+        else:
+            longest_run = max(longest_run, current_run)
+            current_run = 0
+        orzel_w_poprzednim_rzucie = wypadl_orzel
+    return longest_run
