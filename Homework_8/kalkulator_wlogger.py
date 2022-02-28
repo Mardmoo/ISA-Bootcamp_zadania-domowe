449c309daad01ae226b222545916d12bca10b5be
Marek Dmowski <88617623+Mardmoo@users.noreply.github.com>
2022-02-28 14:23:42 +0100

    Add files via upload

---
+++ Homework_8/kalkulator_wlogger.py
@@ -0,0 +1,47 @@
+import logging
+
+
+def kalkulator():
+    logger = logging.getLogger(__name__)
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    try:
+        print('Wybierz opcję')
+        print('1: Dodaj liczby')
+        print('2: Odejmij liczby')
+        print('3: Pomnóż liczby')
+        print('4: Podziel liczby')
+        print('5: Wyjdź')
+        decyzja = input('Podaj opcję, którą chcesz wybrać: ')
+
+        liczba1 = int(input("podaj pierwszą liczbę: "))
+        liczba2 = int(input("podaj drugą liczbę: "))
+
+        if decyzja == '1':
+            wynik = liczba1 + liczba2
+            print(wynik)
+        elif decyzja == '2':
+            wynik = liczba1 - liczba2
+            print(wynik)
+        elif decyzja == '3':
+            wynik = liczba1 * liczba2
+            print(wynik)
+        elif decyzja == '4':
+            wynik = liczba1 / liczba2
+            print(wynik)
+        elif decyzja == '5':
+            quit()
+
+    except ValueError:
+        logger.warning('nie wpisałeś liczby')
+
+    except ZeroDivisionError:
+        logger.error('nie dziel przez 0')
+
+    else:
+        logger.info('dane wejściowe zostały poprawnie zapisane')
+
+
+if __name__ == '__main__':
+    kalkulator()
